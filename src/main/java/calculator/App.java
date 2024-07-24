6719c695946521bb83e14fe65c968657d7c56a2d
이지택 <mkojibhu@naver.com>
2024-07-24 16:44:55 +0900

    테스트

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,9 +3,9 @@ package calculator;
 public class App {
 
     public static void main(String[] args) {
-    int a = 5;
-    int b = 7;
-        System.out.println(a+b);
+    int a = 7;
+    int b = 11;
+        System.out.println(a*b);
 
 
     }
