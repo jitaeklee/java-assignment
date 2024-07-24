dba5f3b9644501e097400c7c428746c8e5f13534
이지택 <mkojibhu@naver.com>
2024-07-24 16:50:54 +0900

    테스트3

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -5,9 +5,9 @@ public class App {
     public static void main(String[] args) {
     int a = 7;
     int b = 11;
-    int c = 5;
+    int c = 6;
         System.out.println(a*b);
-
+        System.out.println(b-c);
 
     }
 }
