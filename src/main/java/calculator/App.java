77562f38eb6900cf6c292616d38929c76eb8676e
이지택 <mkojibhu@naver.com>
2024-07-29 21:18:39 +0900

    init commit

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -3,36 +3,35 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 
-
 public class App {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int result = 0;
-        int count = 0;
-        ArrayList<Integer> resultArr = new ArrayList<>();
+        Calculator cal = new Calculator();
+        double result = 0;
+        ArrayList<Double> resultArr = new ArrayList<>();
         boolean stop = false;
+
 
         while(stop == false) {
         System.out.print("첫 번째 양의 정수를 입력하세요 : ");
-        int num1 = sc.nextInt();
+            double num1 = sc.nextInt();
         System.out.print("두 번째 양의 정수를 입력하세요 : ");
-        int num2 = sc.nextInt();
+            double num2 = sc.nextInt();
         System.out.print("사칙연산 기호를 입력하세요 : ");
         char operator = sc.next().charAt(0);
 
             if (operator == '+') { // 사칙연산 기호 입력값이 "+"와 일치하면
-                result = num1 + num2; // result 값은 num1 + num2
+                cal.add(num1, num2);
+                result = cal.getResult(); // result 값은 num1 + num2
             } else if (operator == '-') { // 사칙연산 기호 입력값이 "-"와 일치하면
-                result = num1 - num2; // result 값은 num1 - num2
+                cal.subtract(num1, num2); // result 값은 num1 - num2
+                result = cal.getResult();
             } else if (operator == '*') { // 사칙연산 기호 입력값이 "*"와 일치하면
-                result = num1 * num2;
+                cal.multiply(num1, num2);
+                result = cal.getResult();
             } else if (operator == '/') { // 사칙연산 기호 입력값이 "/"와 일치하면
-                if (num2 == 0) { // num2가 0일 경우
-                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 " + num2 + "은 입력될 수 없습니다.");
-                } else {
-                    result = num1 / num2;
-                }
+                cal.divide(num1, num2);
+                result = cal.getResult();
             } else {
                 System.out.println("잘못 입력하셨습니다."); // 그 밖에는 해당 출력
                 System.out.println(resultArr.toString());
@@ -50,7 +49,7 @@ public class App {
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String showstr = sc.next();
             if (showstr.equals("inquiry")) {
-                for(int i:resultArr) {
+                for(double i:resultArr) {
                     System.out.print(i + " ");
                 }
                 System.out.println();
@@ -66,3 +65,5 @@ public class App {
         }
     }
 }
+
+
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,27 @@
 package calculator;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Calculator {
-
+    double result;
+    public double getResult() {
+        return result;
+    }
+    // 덧셈 메서드
+    public void add(double a, double b) {result = a + b;}
+    // 뺄셈 메서드
+    public void subtract(double a, double b) {result = a - b;}
+    // 곱셈 메서드
+    public void multiply(double a, double b) {result = a * b;}
+    // 나눗셈 메서드
+    public void divide(double a, double b) {
+        if (b == 0) {
+            System.out.println("0이 올 수 없음");
+        }
+        result = a / b;}
 }
+
+
+
+
+
