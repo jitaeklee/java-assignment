b322396a98ef73fd10d66b885580ebcfd59b1194
이지택 <mkojibhu@naver.com>
2024-07-30 21:15:18 +0900

    2 - 7 까지 작성

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,4 +1,5 @@
 package calculator;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -9,30 +10,50 @@ public class App {
         boolean stop = false;
 
         while (stop == false) {
-            System.out.print("첫 번째 양의 정수를 입력하세요 : ");
-            double num1 = sc.nextInt();
-            System.out.print("두 번째 양의 정수를 입력하세요 : ");
-            double num2 = sc.nextInt();
-            System.out.print("사칙연산 기호를 입력하세요 : ");
-            char operator = sc.next().charAt(0);
+            System.out.println("사칙연산 or 원의넓이 입력");
+            String input = sc.next();
 
-            switch (operator) { // 사칙연산 기호 입력값이 "+"와 일치하면
-                case ('+'):
-                    cal.add(num1, num2);
+            switch (input) {
+                case ("사칙연산"):
+                    System.out.print("첫 번째 양의 정수를 입력하세요 : ");
+                    double num1 = sc.nextInt();
+                    System.out.print("두 번째 양의 정수를 입력하세요 : ");
+                    double num2 = sc.nextInt();
+                    System.out.print("사칙연산 기호를 입력하세요 : ");
+                    char operator = sc.next().charAt(0);
+
+                    switch (operator) { // 사칙연산 기호 입력값이 "+"와 일치하면
+                        case ('+'):
+                            cal.add(num1, num2);
+                            break;
+                        case ('-'):
+                            cal.subtract(num1, num2);
+                            break;
+                        case ('*'):
+                            cal.multiply(num1, num2);
+                            break;
+                        case ('/'):
+                            cal.divide(num1, num2);
+                            break;
+                        default:
+                            System.out.println("잘못된 연산자입니다");
+                            continue;
+                    }
+                    System.out.println("결과 : " + cal.getResult());
+                    cal.resultArr.add(cal.getResult());
                     break;
-                case ('-'):
-                    cal.subtract(num1, num2);
-                    break;
-                case ('*'):
-                    cal.multiply(num1, num2);
-                    break;
-                case ('/'):
-                    cal.divide(num1, num2);
+                case ("원의넓이"):
+                    System.out.println("반지름을 입력하세요: ");
+                    double radius = sc.nextDouble();
+                    if (radius < 0) {
+                        System.out.println("반지름은 음수일 수 없습니다.");
+                    } else {
+                        cal.calculateCircle(radius);
+                        System.out.println("원의 넓이: " + cal.getArea());
+                        cal.circleResultArr.add(cal.getArea());
+                    }
                     break;
             }
-
-            System.out.println("결과 : " + cal.getResult());
-            cal.resultArr.add(cal.getResult());
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String removestr = sc.next();
@@ -44,17 +65,15 @@ public class App {
             String showstr = sc.next();
             if (showstr.equals("inquiry")) {
                 cal.inquiryResults();
+                cal.inquiryCircleResults();
             }
 
             System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
             String exitStr = sc.next();
             if (exitStr.equals("exit")) {
                 stop = true;
-            } else {
-                stop = false;
             }
         }
-        System.out.println(cal.getResult());
     }
 }
 
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,11 @@
 package calculator;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
     private double result;
+    private double area;
 
     Calculator() {
         result = 0;
@@ -13,17 +15,34 @@ public class Calculator {
         return result;
     }
 
-    public void setResult(double result2) {
-        this.result = result2;
+    public void setResult(double result) {
+        this.result = result;
+    }
+
+    public double getArea() {
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
     }
 
     ArrayList<Double> resultArr = new ArrayList<>();
+    ArrayList<Double> circleResultArr = new ArrayList<>();
 
     public void removeResult() {
         resultArr.remove(0);
     }
+
     public void inquiryResults() {
         for (double i : resultArr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public void inquiryCircleResults() {
+        for (double i : circleResultArr) {
             System.out.print(i + " ");
         }
         System.out.println();
@@ -46,6 +65,10 @@ public class Calculator {
             System.out.println("0이 올 수 없음");
         }
         result = a / b;
+    }
+
+    public void calculateCircle(double radius) {
+        area = Math.PI * radius * radius;
     }
 }
 
