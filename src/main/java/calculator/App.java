7623d3a497f67e055c07c27833586b8425eb917c
이지택 <mkojibhu@naver.com>
2024-07-30 11:18:25 +0900

    2 - 4 까지 작성

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -1,4 +1,5 @@
 package calculator;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -7,43 +8,44 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Calculator cal = new Calculator();
-        ArrayList<Double> resultArr = new ArrayList<>();
         boolean stop = false;
 
-        while(stop == false) {
-        System.out.print("첫 번째 양의 정수를 입력하세요 : ");
+        while (stop == false) {
+            System.out.print("첫 번째 양의 정수를 입력하세요 : ");
             double num1 = sc.nextInt();
-        System.out.print("두 번째 양의 정수를 입력하세요 : ");
+            System.out.print("두 번째 양의 정수를 입력하세요 : ");
             double num2 = sc.nextInt();
-        System.out.print("사칙연산 기호를 입력하세요 : ");
-        char operator = sc.next().charAt(0);
+            System.out.print("사칙연산 기호를 입력하세요 : ");
+            char operator = sc.next().charAt(0);
 
-            if (operator == '+') { // 사칙연산 기호 입력값이 "+"와 일치하면
-                cal.add(num1, num2);
-            } else if (operator == '-') { // 사칙연산 기호 입력값이 "-"와 일치하면
-                cal.subtract(num1, num2); // result 값은 num1 - num2
-            } else if (operator == '*') { // 사칙연산 기호 입력값이 "*"와 일치하면
-                cal.multiply(num1, num2);
-            } else if (operator == '/') { // 사칙연산 기호 입력값이 "/"와 일치하면
-                cal.divide(num1, num2);
-            } else {
-                System.out.println("잘못 입력하셨습니다."); // 그 밖에는 해당 출력
-                System.out.println(resultArr.toString());
-                break;
+            switch (operator) { // 사칙연산 기호 입력값이 "+"와 일치하면
+                case ('+'):
+                    cal.add(num1, num2);
+                    break;
+                case ('-'):
+                    cal.subtract(num1, num2);
+                    break;
+                case ('*'):
+                    cal.multiply(num1, num2);
+                    break;
+                case ('/'):
+                    cal.divide(num1, num2);
+                    break;
             }
+
             System.out.println("결과 : " + cal.getResult());
-            resultArr.add(cal.getResult());
+            cal.resultArr.add(cal.getResult());
 
             System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력 시 삭제)");
             String removestr = sc.next();
             if (removestr.equals("remove")) {
-            resultArr.remove(0);
+                cal.removeResult();
             }
 
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
             String showstr = sc.next();
             if (showstr.equals("inquiry")) {
-                for(double i:resultArr) {
+                for (double i : cal.resultArr) {
                     System.out.print(i + " ");
                 }
                 System.out.println();
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,23 +1,43 @@
 package calculator;
+
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Calculator {
     private double result = 0;
-    public double getResult() { return result; }
-    public void setResult(double result2) { this.result = result2; }
-    // 덧셈 메서드
-    public void add(double a, double b) {result = a + b;}
-    // 뺄셈 메서드
-    public void subtract(double a, double b) {result = a - b;}
-    // 곱셈 메서드
-    public void multiply(double a, double b) {result = a * b;}
-    // 나눗셈 메서드
+
+    public double getResult() {
+        return result;
+    }
+
+    public void setResult(double result2) {
+        this.result = result2;
+    }
+
+    ArrayList<Double> resultArr = new ArrayList<>();
+
+    public void removeResult() {
+        resultArr.remove(0);
+    }
+
+    public void add(double a, double b) {
+        result = a + b;
+    }
+
+    public void subtract(double a, double b) {
+        result = a - b;
+    }
+
+    public void multiply(double a, double b) {
+        result = a * b;
+    }
+
     public void divide(double a, double b) {
         if (b == 0) {
             System.out.println("0이 올 수 없음");
         }
-        result = a / b;}
+        result = a / b;
+    }
 }
 
 
